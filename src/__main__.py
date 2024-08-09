4f2c813065b7e8854773a192a575fdbb2087221b
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-08-09 06:27:09 +0200

    Main handle creation of system logging (main handle prod, cli the 'interface', app the server web)

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,14 @@
-import logging
 import sys
 from pathlib import Path
 
 from dotenv import load_dotenv
+from loguru import logger
 
 # Automatically add the working directory
 path = Path(__file__).parents[1].absolute()
 sys.path.append(f"{path}")
+
+from _logging import Level, setup_logger, setup_security  # noqa: E402
 
 from src.cli import cli  # noqa: E402
 
@@ -24,14 +26,20 @@ def main():
     # Load the environment variables
     load_dotenv()
 
-    # Add the user command to the logs
-    logging.debug(f"Command use \"{' '.join(sys.argv)}\"")
+    # Load the logging system
+    setup_logger(level=Level.DEBUG)
+
+    # Setup the security for the logging folder
+    setup_security(desired_permissions=0o750)
+
+    # Add the user command to the logs (first is src path)
+    logger.info(f"Command for launch application is \"{' '.join(sys.argv[1:])}\"")
 
     try:
         cli()
         # Typer have his own exception handling
     except KeyboardInterrupt as exception:
-        logging.debug(f"Exiting the program: '{exception}'")
+        logger.debug(f"Exiting the program: '{exception}'")
 
 
 if __name__ == "__main__":
